703fc38e68a0f11ac91d83e035cb64480d90748b
Viacheslav <east.slava@gmail.com>
2025-08-10 15:15:20 +1200

    Added Jenkinsfile

--- SimpleGitProj/obj/Debug/net9.0/SimpleGitProj.AssemblyInfo.cs
+++ SimpleGitProj/obj/Debug/net9.0/SimpleGitProj.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SimpleGitProj")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f539027463986c7e6c72c263e50fd3ae70524f8")]
 [assembly: System.Reflection.AssemblyProductAttribute("SimpleGitProj")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SimpleGitProj")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
